0096e23ce49a8b105a6dfdcdba92d49d8e681c27
AbdiwahabMohamed <fahadfar69@gmail.com>
2024-12-05 13:51:43 +0300

    Navigating

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+
+
+function CustomerPage() {
+    return (
+        <div>CustomerPage</div>
+    )
+}
+
+export default CustomerPage
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+
+
+function InvoicePage() {
+    return (
+        <div>InvoicePage</div>
+    )
+}
+
+export default InvoicePage
